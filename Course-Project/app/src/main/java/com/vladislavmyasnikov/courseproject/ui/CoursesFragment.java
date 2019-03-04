c5d94701c4b90b340c90cbbbe3ce51c8a6e91a3c
Vladislav76 <vladislav.myasnikov@bk.ru>
2019-03-04 23:03:44 +0300

    Initial commit

--- Course-Project/app/src/main/java/com/vladislavmyasnikov/courseproject/ui/CoursesFragment.java
+++ Course-Project/app/src/main/java/com/vladislavmyasnikov/courseproject/ui/CoursesFragment.java
@@ -4,8 +4,9 @@ import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,7 +18,7 @@ import com.vladislavmyasnikov.courseproject.ui.callbacks.OnFragmentListener;
 public class CoursesFragment extends Fragment {
 
     @Override
-    public void onAttach(Context context) {
+    public void onAttach(@NonNull Context context) {
         super.onAttach(context);
         if (context instanceof OnFragmentListener) {
             mFragmentListener = (OnFragmentListener) context;
@@ -36,6 +37,19 @@ public class CoursesFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         mFragmentListener.setToolbarTitle(R.string.courses_toolbar_title);
+
+        addChildFragment(CONTENT_FRAME_1_TAG);
+        addChildFragment(CONTENT_FRAME_2_TAG);
+        addChildFragment(CONTENT_FRAME_3_TAG);
+
+        final SwipeRefreshLayout layout = view.findViewById(R.id.swipe_refresh_layout);
+        layout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                mAcademicPerformanceFragment.updateBadges();
+                layout.setRefreshing(false);
+            }
+        });
     }
 
     @Override
@@ -44,9 +58,41 @@ public class CoursesFragment extends Fragment {
         mFragmentListener = null;
     }
 
+    private void addChildFragment(String tag) {
+        FragmentManager fragmentManager = getChildFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentByTag(CONTENT_FRAME_1_TAG);
+        int containerId;
+
+        if (fragment == null) {
+            switch (tag) {
+                case CONTENT_FRAME_1_TAG:
+                    mAcademicPerformanceFragment = AcademicPerformanceFragment.newInstance();
+                    fragment = mAcademicPerformanceFragment;
+                    containerId = R.id.content_frame_1;
+                    break;
+                case CONTENT_FRAME_2_TAG:
+                    fragment = RatingFragment.newInstance();
+                    containerId = R.id.content_frame_2;
+                    break;
+                case CONTENT_FRAME_3_TAG:
+                    fragment = PassedCoursesFragment.newInstance();
+                    containerId = R.id.content_frame_3;
+                    break;
+                default:
+                    return;
+            }
+            fragmentManager.beginTransaction().replace(containerId, fragment).commit();
+        }
+    }
+
     public static CoursesFragment newInstance() {
         return new CoursesFragment();
     }
 
     private OnFragmentListener mFragmentListener;
+    private AcademicPerformanceFragment mAcademicPerformanceFragment;
+
+    private static final String CONTENT_FRAME_1_TAG = "content_frame_1";
+    private static final String CONTENT_FRAME_2_TAG = "content_frame_2";
+    private static final String CONTENT_FRAME_3_TAG = "content_frame_3";
 }
