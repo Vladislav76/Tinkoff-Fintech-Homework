e59b0b7b45ab66f87b6380ebd1618692926ff323
Vladislav76 <vladislav.myasnikov@bk.ru>
2019-03-20 22:27:46 +0300

    Initial commit

--- Course-Project/app/src/main/java/com/vladislavmyasnikov/courseproject/ui/AcademicPerformanceFragment.java
+++ Course-Project/app/src/main/java/com/vladislavmyasnikov/courseproject/ui/AcademicPerformanceFragment.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,8 +20,23 @@ import java.util.Random;
 
 public class AcademicPerformanceFragment extends Fragment {
 
+    private static final String UPDATED_POINTS_DATA = "updated_points_data";
+    private static final int CURRENT_HARDCODED_NUMBER_OF_USER_ICONS = 2;
+
     private UserView mUser1View;
     private UserView mUser2View;
+    private Handler mHandler = new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            Bundle data = msg.getData();
+            int[] points = data.getIntArray(UPDATED_POINTS_DATA);
+            if (points != null) {
+                mUser1View.setBadgeCount(points[0]);
+                mUser2View.setBadgeCount(points[1]);
+            }
+        }
+    };
 
     private View.OnClickListener mOnTitleListener = new View.OnClickListener() {
         @Override
@@ -40,14 +58,22 @@ public class AcademicPerformanceFragment extends Fragment {
         view.findViewById(R.id.title).setOnClickListener(mOnTitleListener);
     }
 
-    private void updateBadgeValue(@NonNull UserView view, int newValue) {
-        view.setBadgeCount(newValue);
-    }
-
     public void updateBadges() {
-        Random random = new Random();
-        updateBadgeValue(mUser1View, random.nextInt(11));
-        updateBadgeValue(mUser2View, random.nextInt(11));
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Message message = Message.obtain();
+                Bundle data = new Bundle();
+                Random random = new Random();
+                int[] points = new int[CURRENT_HARDCODED_NUMBER_OF_USER_ICONS];
+                for (int i = 0; i < points.length; i++) {
+                    points[i] = random.nextInt(11);
+                }
+                data.putIntArray(UPDATED_POINTS_DATA, points);
+                message.setData(data);
+                mHandler.sendMessage(message);
+            }
+        }).start();
     }
 
     public static AcademicPerformanceFragment newInstance() {
